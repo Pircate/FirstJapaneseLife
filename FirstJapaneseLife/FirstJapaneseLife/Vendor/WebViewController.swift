62462c283e4126961efdedf5c9f0d20a70cd99f9
gaoxiang <gao497868860@163.com>
2017-12-24 01:32:57 +0800

    update web view status bar style

--- FirstJapaneseLife/FirstJapaneseLife/Vendor/WebViewController.swift
+++ FirstJapaneseLife/FirstJapaneseLife/Vendor/WebViewController.swift
@@ -87,6 +87,10 @@ class WebViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     // MARK: - private
     private func addObserver() {
