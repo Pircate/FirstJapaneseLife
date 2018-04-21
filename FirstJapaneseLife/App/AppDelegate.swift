0034bd146ae4475f7fec5cdba3bdc43c7ed60dd5
Gin <gao497868860@163.com>
2018-04-21 16:16:35 +0800

    update pod

---
+++ FirstJapaneseLife/App/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  FirstJapaneseLife
+//
+//  Created by G-Xi0N on 2017/12/13.
+//  Copyright © 2017年 G-Xi0N. All rights reserved.
+//
+
+import UIKit
+import EachNavigationBar
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        UIViewController.setupNavigationBar
+        LanguageManager.shared.isJapanese = true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        let nav = UINavigationController(rootViewController: MainViewController())
+        nav.navigation.configuration.enabled = true
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, ’8.0’
+use_frameworks!
+
+target 'FirstJapaneseLife' do
+    pod 'SnapKit'
+    pod 'EachNavigationBar'
+    pod 'FDFullscreenPopGesture'
+    pod 'Agrume'
+    pod 'HandyJSON'
+    pod 'SDCycleScrollView'
+end
